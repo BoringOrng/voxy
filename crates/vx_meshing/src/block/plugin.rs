a279ced812ec539e00e275e8370059f46a809d67
BoringOrng <BoringOrng@users.noreply.github.com>
2026-08-06 00:30:28 -0500

    refactor: trait-based resource-loader

--- crates/vx_meshing/src/block/plugin.rs
+++ crates/vx_meshing/src/block/plugin.rs
@@ -1,7 +1,7 @@
 use bevy::prelude::*;
-use vx_mod_resource::BlockTextureArray;
+use vx_mod_resource::{loader, registry};
 
-use crate::block::{BlockMaterial, material::SharedBlockMaterial};
+use crate::block::{self, BlockMaterial, material::SharedBlockMaterial};
 
 #[derive(Default)]
 pub struct BlockMeshingPlugin;
@@ -12,7 +12,7 @@ impl BlockMeshingPlugin {
         reason = "`If<Res<BlockTextureArray>>` must be passed by value as is required by bevy"
     )]
     fn setup_block_material(
-        block_texture_array: If<Res<BlockTextureArray>>,
+        block_texture_array: If<Res<block::TextureArray>>,
         mut materials: ResMut<Assets<BlockMaterial>>,
         mut commands: Commands,
     ) {
@@ -22,11 +22,34 @@ impl BlockMeshingPlugin {
 
         commands.insert_resource(SharedBlockMaterial::new(handle));
     }
+
+    #[expect(
+        clippy::needless_pass_by_value,
+        reason = "
+            `Res<vx_mod_resource::Registry<loader::TextureLoader>>`,
+            `Res<AssetServer>`, and `Res<Assets<Image>>` must be passed by value
+            as is required by bevy
+        "
+    )]
+    fn setup_block_texture_array(
+        texture_registry: Res<vx_mod_resource::Registry<loader::TextureLoader>>,
+        asset_server: Res<AssetServer>,
+        images: Res<Assets<Image>>,
+        mut commands: Commands,
+    ) {
+        let texture_array = block::TextureArray::build(&texture_registry, &asset_server, &images);
+        commands.insert_resource(texture_array);
+    }
 }
 
 impl Plugin for BlockMeshingPlugin {
     fn build(&self, app: &mut App) {
         app.add_plugins(MaterialPlugin::<BlockMaterial>::default())
+            .add_systems(
+                PreUpdate,
+                Self::setup_block_texture_array
+                    .run_if(resource_added::<registry::Ready<loader::TextureLoader>>),
+            )
             .add_systems(
                 Update,
                 Self::setup_block_material.run_if(not(resource_exists::<SharedBlockMaterial>)),
--- crates/vx_mod_resource/src/lib.rs
+++ crates/vx_mod_resource/src/lib.rs
@@ -1,16 +1,8 @@
+pub mod loader;
 pub mod registry;
 
-mod loader;
 mod plugins;
 
 pub(crate) use loader::Loader;
-
-mod block_array;
-mod plugin;
-mod texture_map;
-
-pub use block_array::BlockTextureArray;
-pub use plugin::ModResourcePlugin;
 pub use plugins::Plugins;
 pub use registry::Registry;
-pub use texture_map::TextureRegistry;
