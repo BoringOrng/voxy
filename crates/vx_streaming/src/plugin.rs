853b2bf5285c9e66e33890cfeb5aabc5a9007e19
BoringOrng <BoringOrng@users.noreply.github.com>
2026-08-07 23:10:22 -0500

    feat: chunk loading

--- crates/vx_streaming/src/plugin.rs
+++ crates/vx_streaming/src/plugin.rs
@@ -1,4 +1,4 @@
-use bevy::prelude::*;
+use bevy::{math::bounding::Aabb3d, platform::collections::HashSet, prelude::*};
 use vx_world::chunk::{ChunkMap, ChunkPos};
 
 pub struct StreamingPlugin;
@@ -33,11 +33,47 @@ impl StreamingPlugin {
             info!("dropped chunk at {drop_pos:?}");
         }
     }
+
+    #[expect(
+        clippy::needless_pass_by_value,
+        reason = "`Res<ChunkMap>` and `Res<super::Config>` must be passed by value as is required by bevy"
+    )]
+    fn spawn_chunks(
+        mut commands: Commands,
+        chunk_map: Res<ChunkMap>,
+        stream_config: Res<super::Config>,
+        anchor_positions: Query<&ChunkPos, With<super::Anchor>>,
+    ) {
+        let spawn_radius = stream_config.load_radius();
+        let to_ignore: &HashSet<_> = &chunk_map.keys().copied().collect();
+
+        let to_spawn: Vec<_> = anchor_positions
+            .iter()
+            .flat_map(|&anchor_pos| {
+                let spawn_bounds =
+                    Aabb3d::new(anchor_pos.as_vec3a(), Vec3A::splat(spawn_radius as f32));
+
+                let min = spawn_bounds.min.as_ivec3();
+                let max = spawn_bounds.max.as_ivec3();
+
+                (min.x..=max.x)
+                    // two flat maps here can be read as
+                    // `for x in .. { for y in .. { for z in .. { (x, y, z) } } }`
+                    .flat_map(move |x| (min.y..=max.y).map(move |y| (x, y)))
+                    .flat_map(move |(x, y)| (min.z..=max.z).map(move |z| (x, y, z)))
+                    .map(|(x, y, z)| ChunkPos::new(IVec3::new(x, y, z)))
+                    .filter(move |pos| !to_ignore.contains(pos))
+            })
+            .map(|chunk_pos| (chunk_pos, super::PendingGeneration))
+            .collect();
+
+        commands.spawn_batch(to_spawn);
+    }
 }
 
 impl Plugin for StreamingPlugin {
     fn build(&self, app: &mut App) {
         app.init_resource::<super::Config>()
-            .add_systems(FixedUpdate, Self::despawn_chunks);
+            .add_systems(FixedUpdate, (Self::despawn_chunks, Self::spawn_chunks));
     }
 }
--- crates/vx_world/src/chunk/chunk_pos.rs
+++ crates/vx_world/src/chunk/chunk_pos.rs
@@ -5,6 +5,11 @@ pub struct ChunkPos(IVec3);
 
 impl ChunkPos {
     #[must_use]
+    pub const fn new(pos: IVec3) -> Self {
+        Self(pos)
+    }
+
+    #[must_use]
     pub fn from_vec3(pos: Vec3) -> Self {
         Self(pos.as_ivec3().div_euclid(super::Chunk::SIZE.as_ivec3()))
     }
