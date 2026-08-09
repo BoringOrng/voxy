6160f3ce106ff17eb99645d668c80eeb1cdb8db0
BoringOrng <BoringOrng@users.noreply.github.com>
2026-08-09 17:41:18 -0500

    feat: async chunk streaming

--- crates/vx_streaming/src/plugin.rs
+++ crates/vx_streaming/src/plugin.rs
@@ -1,68 +1,128 @@
-use bevy::{math::bounding::Aabb3d, platform::collections::HashSet, prelude::*};
+use bevy::{
+    math::bounding::Aabb3d, platform::collections::HashSet, prelude::*, tasks::AsyncComputeTaskPool,
+};
+use crossbeam_channel::{Receiver, Sender};
 use vx_world::chunk::{Chunk, ChunkMap, ChunkPos, chunk_state};
 
 pub struct StreamingPlugin;
 
+#[derive(Resource)]
+struct DespawnChannel {
+    sender: Sender<Entity>,
+    receiver: Receiver<Entity>,
+}
+
+#[derive(Resource)]
+struct SpawnChannel {
+    sender: Sender<ChunkPos>,
+    receiver: Receiver<ChunkPos>,
+}
+
 impl StreamingPlugin {
+    fn setup_channels(mut commands: Commands) {
+        let (despawn_sender, despawn_receiver) = crossbeam_channel::unbounded();
+        let (spawn_sender, spawn_receiver) = crossbeam_channel::unbounded();
+
+        commands.insert_resource(DespawnChannel {
+            sender: despawn_sender,
+            receiver: despawn_receiver,
+        });
+
+        commands.insert_resource(SpawnChannel {
+            sender: spawn_sender,
+            receiver: spawn_receiver,
+        });
+    }
+
     #[expect(
         clippy::needless_pass_by_value,
-        reason = "`Res<super::Config>` must be passed by value as is required by bevy"
+        reason = "
+            `Res<DespawnChannel>`, `Res<ChunkMap>`, and `Res<super::Config>`
+            must be passed by value as is required by bevy
+        "
     )]
-    fn despawn_chunks(
-        mut chunk_map: ResMut<ChunkMap>,
-        mut commands: Commands,
+    fn start_despawn(
+        channel: Res<DespawnChannel>,
+        chunk_map: Res<ChunkMap>,
         stream_config: Res<super::Config>,
         anchor_positions: Query<&ChunkPos, With<super::Anchor>>,
     ) {
-        let drop_dist = stream_config.unload_radius();
+        let unload_radius = stream_config.unload_radius();
+        let pool = AsyncComputeTaskPool::get();
 
-        let to_drop: Vec<_> = anchor_positions
-            .iter_inner()
-            .flat_map(|&anchor_pos| {
-                chunk_map
+        for &anchor_pos in &anchor_positions {
+            let sender = channel.sender.clone();
+            let chunk_map = chunk_map.clone();
+
+            pool.spawn(async move {
+                for &chunk in chunk_map
                     .keys()
-                    .filter(move |chunk_pos| chunk_pos.chebyshev_distance(*anchor_pos) >= drop_dist)
-                    .copied()
+                    .filter(|chunk_pos| chunk_pos.chebyshev_distance(*anchor_pos) >= unload_radius)
+                    .filter_map(|chunk_pos| chunk_map.get(chunk_pos))
+                {
+                    _ = sender.send(chunk);
+                }
             })
-            .collect();
-
-        for drop_pos in to_drop {
-            let chunk_entity = chunk_map.remove(&drop_pos).unwrap();
-            commands.entity(chunk_entity).despawn();
+            .detach();
         }
     }
 
     #[expect(
         clippy::needless_pass_by_value,
-        reason = "`Res<ChunkMap>` and `Res<super::Config>` must be passed by value as is required by bevy"
+        reason = "`Res<DespawnChannel>` must be passed by value as is required by bevy"
     )]
-    fn spawn_chunks(
-        mut commands: Commands,
+    fn handle_despawn(channel: Res<DespawnChannel>, mut commands: Commands) {
+        for msg in channel.receiver.try_iter() {
+            // have to do try_despawn here because our pool might contain multiple of the
+            // same entity
+            commands.entity(msg).try_despawn();
+        }
+    }
+
+    fn start_spawn(
+        channel: Res<SpawnChannel>,
         chunk_map: Res<ChunkMap>,
         stream_config: Res<super::Config>,
         anchor_positions: Query<&ChunkPos, With<super::Anchor>>,
     ) {
-        let spawn_radius = stream_config.load_radius();
-        let to_ignore: &HashSet<_> = &chunk_map.keys().copied().collect();
+        let load_radius = UVec3::splat(stream_config.load_radius());
+        let pool = AsyncComputeTaskPool::get();
 
-        let to_spawn: Vec<_> = anchor_positions
-            .iter()
-            .flat_map(|&anchor_pos| {
-                let spawn_bounds =
-                    Aabb3d::new(anchor_pos.as_vec3a(), Vec3A::splat(spawn_radius as f32));
+        let to_ignore: HashSet<_> = chunk_map.keys().copied().collect();
 
+        for &anchor_pos in &anchor_positions {
+            let sender = channel.sender.clone();
+            let to_ignore = to_ignore.clone();
+
+            pool.spawn(async move {
+                let spawn_bounds = Aabb3d::new(anchor_pos.as_vec3a(), load_radius.as_vec3a());
                 let min = spawn_bounds.min.as_ivec3();
                 let max = spawn_bounds.max.as_ivec3();
 
-                (min.x..=max.x)
+                for chunk_pos in (min.x..=max.x)
                     // two flat maps here can be read as
                     // `for x in .. { for y in .. { for z in .. { (x, y, z) } } }`
                     .flat_map(move |x| (min.y..=max.y).map(move |y| (x, y)))
                     .flat_map(move |(x, y)| (min.z..=max.z).map(move |z| (x, y, z)))
                     .map(|(x, y, z)| ChunkPos::new(IVec3::new(x, y, z)))
                     .filter(move |pos| !to_ignore.contains(pos))
+                {
+                    _ = sender.send(chunk_pos);
+                    // Transform::from_translation(chunk_pos.as_vec3() *
+                    // Chunk::SIZE.as_vec3()),
+                }
             })
-            .map(|chunk_pos| {
+            .detach();
+        }
+    }
+
+    fn handle_spawn(channel: Res<SpawnChannel>, chunk_map: Res<ChunkMap>, mut commands: Commands) {
+        let unique_chunks: HashSet<_> = channel.receiver.try_iter().collect();
+
+        let to_spawn: Vec<_> = unique_chunks
+            .iter()
+            .filter(|&chunk_pos| !chunk_map.contains_key(chunk_pos))
+            .map(|&chunk_pos| {
                 (
                     chunk_pos,
                     Transform::from_translation(chunk_pos.as_vec3() * Chunk::SIZE.as_vec3()),
@@ -78,6 +138,16 @@ impl StreamingPlugin {
 impl Plugin for StreamingPlugin {
     fn build(&self, app: &mut App) {
         app.init_resource::<super::Config>()
-            .add_systems(FixedUpdate, (Self::despawn_chunks, Self::spawn_chunks));
+            .add_systems(Startup, Self::setup_channels)
+            .add_systems(
+                FixedUpdate,
+                (
+                    Self::start_despawn,
+                    Self::handle_despawn,
+                    Self::start_spawn,
+                    Self::handle_spawn,
+                )
+                    .after(Self::setup_channels),
+            );
     }
 }
